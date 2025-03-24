149d1193a39422a296490f6aefc06ad7aa3059f0
Ritik303 <100149666+Ritik303@users.noreply.github.com>
2025-03-24 22:14:34 +0530

    Added Regex for valid username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
   import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { Data, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -22,5 +22,15 @@ export class AppComponent {
     console.log("Save button is clicked", $event);
     window.open(this.url,"blank");
   }
- 
+  onInput($event :any){
+    console.log("Change Event occured!", $event.data);
+    const nameRegex = RegExp('[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.username)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";    
+  }
+
+
 }
